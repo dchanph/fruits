01e61fb81fdb6f445996b40eddedc927c9fed467
Doreen Chan <doreenchan@Doreens-MacBook-Pro.local>
2019-10-30 19:26:14 +0800

    push fruits docs

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
 // Dependencies
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // middleware
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: false}))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 // 'Data'
-const fruits = require('./models/fruits.js')
+const fruits = require("./models/fruits.js");
 
 // Index
-app.get('/fruits', (req, res) => {
-  res.render('index.ejs', {
+app.get("/fruits", (req, res) => {
+  res.render("index.ejs", {
     fruits: fruits
-  })
-})
+  });
+});
 
 // New
-app.get('/fruits/new', (req, res) => {
-  res.render('new.ejs');
-})
+app.get("/fruits/new", (req, res) => {
+  res.render("new.ejs");
+});
 
 // Show
-app.get('/fruits/:index', (req, res) => {
-  const currentFruit = fruits[req.params.index]
-  res.render('show.ejs', {
+app.get("/fruits/:index", (req, res) => {
+  const currentFruit = fruits[req.params.index];
+  res.render("show.ejs", {
     fruit: currentFruit
-  })
-})
+  });
+});
 
 // Create
-app.post('/fruits', (req, res) => {
-  if (req.body.readyToEat === 'on') { // if checked, req.body.readyToEat is set to 'on'
-    req.body.readyToEat = true
-  } else { // if not checked, req.body.readyToEat is undefined
-    req.body.readyToEat = false
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    // if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true;
+  } else {
+    // if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false;
   }
-  fruits.push(req.body)
-  res.redirect('/fruits')
-})
+  fruits.push(req.body);
+  res.redirect("/fruits");
+});
 
 app.listen(PORT, () => {
-  console.log('listening on port', PORT)
-})
+  console.log("listening on port", PORT);
+});
